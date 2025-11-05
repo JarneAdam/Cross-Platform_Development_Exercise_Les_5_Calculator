d22fe6378d6d619e3ccf8f96ecad1fd9af850a20
Jarne Adam <jarne.adam@student.vives.be>
2025-11-05 12:47:45 +0100

    Added component display and fixed imports

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Display from './src/components/Display';
+import theme from './src/styles/theme.styles';
 
 export default function App() {
   return (
     <View style={styles.container}>
-
+      <Display />
       <StatusBar style="auto" />
     </View>
   );
---
+++ src/components/Display.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const Display = () => {
+    return (
+        <Text style={styles.display}>123456789</Text>
+    );
+};
+
+const styles = StyleSheet.create({
+    display: {
+        color: 'white',
+        fontSize: 40,
+        textAlign: 'right',
+        marginTop: 10,
+        marginRight: 20,
+    }
+});
+
+export default Display;
